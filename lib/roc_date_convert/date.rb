322008f4cd2ac83e72e816d2de880625f509e4ee
Archer <twbcs@yahoo.com.tw>
2016-12-09 15:42:32 +0800

    fix to instance

--- lib/roc_date_convert/date.rb
+++ lib/roc_date_convert/date.rb
@@ -3,8 +3,8 @@ require 'active_support/core_ext'
 
 class Date
 
-  def roc_date(date)
-    year = date.year - 1911
+  def roc_date
+    year = self.year - 1911
     "#{year}年#{date.month}月#{date.day}日"
   end
 
--- lib/roc_date_convert/time.rb
+++ lib/roc_date_convert/time.rb
@@ -3,14 +3,14 @@ require 'active_support/core_ext'
 
 class Time
 
-  def roc_date(time)
-    ad_time = time.localtime
+  def roc_date
+    ad_time = self.localtime
     year = ad_time.year - 1911
     "#{year}年#{ad_time.month}月#{ad_time.day}日"
   end
 
-  def roc_time(time)
-    ad_time = time.localtime
+  def roc_time
+    ad_time = self.localtime
     year = ad_time.year - 1911
     "#{year}年#{ad_time.month}月#{ad_time.day}日 #{ad_time.hour}:#{ad_time.min}"
   end
