df1c287a6b7f4c332ea7e3844954d39841623de3
Archer <twbcs@yahoo.com.tw>
2016-12-12 10:42:10 +0800

    fix nil error

--- lib/roc_date_convert.rb
+++ lib/roc_date_convert.rb
@@ -1,3 +1,4 @@
 require "roc_date_convert/version"
 require "roc_date_convert/time"
 require "roc_date_convert/date"
+require "roc_date_convert/nil"
---
+++ lib/roc_date_convert/nil.rb
@@ -0,0 +1,8 @@
+class NilClass
+  # 去除噴錯問題
+  def roc_date(front = "")
+  end
+
+  def roc_time(front = "")
+  end
+end
